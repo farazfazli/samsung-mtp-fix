4bee5943716252db36d6c2018caa7a48d7f66f5a
Faraz Fazli <farazfazli@gmail.com>
2016-08-29 10:40:50 -0400

    Update samsungmtpfix.sh

--- samsungmtpfix.sh
+++ samsungmtpfix.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 echo "Close SideSync and/or Kext, and unplug your phone. Press enter to continue..."
 read
+sudo kextunload -b “com.devguru.driver.SamsungComposite” -v 5
+sudo kextunload -b “com.devguru.driver.SamsungACMData” -v 5
+sudo kextunload -b “com.devguru.driver.SamsungMTP” -v 5
+sudo kextunload -b “com.devguru.driver.SamsungACMControl” -v 5
 sudo kextunload -b /System/Library/Extentions/ssuddrv.kext -v 5
 sudo rm -rf /System/Library/Extentions/ssuddrv.kext
