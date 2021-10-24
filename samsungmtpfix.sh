9c8fe14d33f2afad3a127b5a81484cc9e71b348b
Greg Sadetsky <lepetitg@gmail.com>
2021-10-23 21:36:41 -0400

    fix for path to Extensions

--- samsungmtpfix.sh
+++ samsungmtpfix.sh
@@ -5,5 +5,5 @@ sudo kextunload -b com.devguru.driver.SamsungComposite -v 5
 sudo kextunload -b com.devguru.driver.SamsungACMData -v 5
 sudo kextunload -b com.devguru.driver.SamsungMTP -v 5
 sudo kextunload -b com.devguru.driver.SamsungACMControl -v 5
-sudo kextunload -b /System/Library/Extentions/ssuddrv.kext -v 5
-sudo rm -rf /System/Library/Extentions/ssuddrv.kext
+sudo kextunload -b /System/Library/Extensions/ssuddrv.kext -v 5
+sudo rm -rf /System/Library/Extensions/ssuddrv.kext
